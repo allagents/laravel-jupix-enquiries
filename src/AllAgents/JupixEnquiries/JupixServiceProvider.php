8a40a1a331ffa05f2cdbd0a8cfefdd2d74734999
Josh Murray <josh@allagents.co.uk>
2019-03-04 14:01:56 +0000

    Changed some code

--- src/AllAgents/JupixEnquiries/JupixServiceProvider.php
+++ src/AllAgents/JupixEnquiries/JupixServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace AllAgents\JupixEnquiries;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class JupixServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class JupixServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Log::debug('Booted JupixServiceProvider');
     }
 
     /**
